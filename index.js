2780a570611248a1fe332805a016429a2ca3b247
Dominic Black <dpblack96@gmail.com>
2022-01-15 15:10:30 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(work = "go to the office") {
+    return (`This Monday, I will ${work}.`)
+}
+
+function wrapAdjective(flair = '*'){
+    function innerFunction (adjective = 'special'){
+        return (`You are ${flair}${adjective}${flair}!`)
+    }
+    return innerFunction
+}
+wrapAdjective ('||')('a dedicated programmer')
